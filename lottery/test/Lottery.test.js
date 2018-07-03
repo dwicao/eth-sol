082cde672dcf368ab4e62f40aa1b2ef15f3597e4
dwicao <lutfiandwica@gmail.com>
2018-07-04 06:26:08 +0700

    add test sends money to the winner

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -79,4 +79,20 @@ describe('Lottery', () => {
       assert(err);
     }
   });
+
+  it('sends money to the winner', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether'),
+    });
+
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+    await lottery.methods.pickWinner().send({from: accounts[0]});
+
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
+  });
 });
