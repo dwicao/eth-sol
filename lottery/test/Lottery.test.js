a1625250d24e18a9670879a2896649faebdd9a23
dwicao <lutfiandwica@gmail.com>
2018-07-04 04:34:32 +0700

    add lottery contract test

---
+++ lottery/test/Lottery.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const {interface, bytecode} = require('../compile');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+describe('Lottery', () => {
+  let accounts, lottery;
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    lottery = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({data: bytecode})
+      .send({from: accounts[0], gas: '1000000'});
+  });
+
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+});
