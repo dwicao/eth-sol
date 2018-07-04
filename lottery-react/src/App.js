39a0a54519c95a35511512eeaefbbafc75fe8f48
dwicao <lutfiandwica@gmail.com>
2018-07-04 20:56:41 +0700

    get current lottery state

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import lottery from './lottery';
+import web3 from './web3';
 
 class App extends Component {
+  state = {
+    manager: '',
+    balance: '',
+    players: [],
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({manager, players, balance});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Hello world</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <p>There are currently {this.state.players.length} people entered</p>
+        <p>
+          Competing to win {web3.utils.fromWei(this.state.balance, 'ether')}{' '}
+          ether
         </p>
       </div>
     );
